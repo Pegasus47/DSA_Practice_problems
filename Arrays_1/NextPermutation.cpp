b9a5c28d191528f3ee79c4bde0561813a5ae67c1
unknown <arpitkudre@gmail.com>
2025-05-30 22:49:04 +0530

    feat: Q3 solution

---
+++ Arrays_1/NextPermutation.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int bp=-1;
+        for(int i=nums.size()-1;i > 0;--i){ //check notes for more detailed solutions
+            if(nums[i-1]<nums[i]){
+                bp=i-1;
+                break;
+            }
+        }
+        if(bp==-1){
+            sort(nums.begin(),nums.end());
+            return;
+        }
+        sort(nums.begin()+bp+1,nums.end());
+        for(int i=bp+1;i<nums.size();i++){
+            if(nums[i]>nums[bp]){
+                int temp=nums[bp];
+                nums[bp]=nums[i];
+                nums[i]=temp;
+                break;
+            }
+        }
+    }
+};
+
+int main() {
+    Solution s;
+    vector<int> v={1,2,3,4,5};
+    s.nextPermutation(v);
+    for (int i:v) { //this to seem cool
+        cout << i;
+    }
+    return 0;
+}
