e249b5c628f34d464d156c11f2366f9827ded6f8
unknown <arpitkudre@gmail.com>
2025-06-16 20:18:03 +0530

    longest sequence

---
+++ Arrays_4/LongestConsecutiveSequence.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        if(nums.empty()) return 0;
+        int len=1;
+        unordered_set <int> us;
+        for(int i=0;i<nums.size();i++){
+            us.insert(nums[i]);
+        }
+        for(auto it:us){
+            if(us.find(it-1)==us.end()){
+                int cnt=1;
+                int x=it;
+                while(us.find(x+1)!=us.end()) {
+                    x++;
+                    cnt++;
+                }
+                len=max(len,cnt);
+            }
+        }
+        return len;
+    }
+};
+
+int main() {
+    Solution s;
+    vector<int> v={1,3,5,3143,843,200,4,69,2};
+    cout << s.longestConsecutive(v) << endl     ;
+}
